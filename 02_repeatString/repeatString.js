3ea48b95c6c0d2c7e175a72936c44cc11d083b61
Tiong Lee <LLLTTH@gmail.com>
2022-11-09 15:14:48 +0800

    updated the code with loops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, i) {
+    var repeatedWord = "";
+    if (i < 0) {
+        return "ERROR";
+    };
+    while (i > 0) {
+        repeatedWord += word;
+        i--;
 };
+return repeatedWord;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
