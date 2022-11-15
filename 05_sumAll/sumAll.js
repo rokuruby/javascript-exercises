b4c0ec990cddbae630031b44bf62eb4c6fdf482b
Tiong Lee <LLLTTH@gmail.com>
2022-11-15 15:44:40 +0800

    added some copypasta code for this impossible shit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(arr) {
+    let fullArr = [];
+    let sum = 0;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+    arr.sort(function(a,b) {
+        return a-b
+    });
+
+    for (let i = arr[0]; i <=arr[1]; i++) {
+        fullArr.push(i);
+    }
+    sum = fullArr.reduce(reducer);
+
+    return sum;
 };
 
 // Do not edit below this line
