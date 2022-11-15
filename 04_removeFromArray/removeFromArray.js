6e692914182b8098dc9f36b39bb13b39bbfd7ab8
Tiong Lee <LLLTTH@gmail.com>
2022-11-15 14:04:30 +0800

    added some code for splicing arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,b,c,d,e) {
+   let array = a;
+   array.splice(b-1,1);
+   array.splice(c-1,1);
+   array.splice(d-1,1);
+   array.splice(e-1,1);
+   return array;
+      
 };
 
 // Do not edit below this line
