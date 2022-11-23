20804cbeb003025316f56c89c5bced96173b5bf0
Tiong Lee <LLLTTH@gmail.com>
2022-11-23 14:53:27 +0800

    added the code to pass all the test for leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    var lYear = year;
+    if (lYear % 400 == 0 && lYear % 4 == 0) {return true;}
+    
+    else if (lYear % 4 == 0 && lYear % 100 > 0) {return true;}
 
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
