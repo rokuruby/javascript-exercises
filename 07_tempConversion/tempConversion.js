c3266afcc22b2be068353bd951aedcffd03b9905
Tiong Lee <LLLTTH@gmail.com>
2022-11-26 14:56:39 +0800

    added c to F and F to C code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+    var convertC = (f - 32) * (5/9);
+  var decimalC = parseFloat(convertC.toFixed(1));
+  return decimalC;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  var convertF = (c * (9/5)) + 32;
+  var decimalF = parseFloat(convertF.toFixed(1));
+  return decimalF;
 };
 
 // Do not edit below this line
