db881f93c51ba4aa2e3c3dd1ccb927d484dcbdcd
Tiong Lee <LLLTTH@gmail.com>
2022-11-26 22:33:44 +0800

    googled and implmented Binet's Fibonacci formula into the code to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  //using Binet's formula so that its not recursive.This is so I can just plug a fib sequence and formula will spit our the fib number isntead of manually calculating every fib number aka reecursive
+  
+  if (num < 0) {return "OOPS"};
+    let fib = ((((1+(Math.sqrt(5)))/2)**(num))-((-((1+(Math.sqrt(5)))/2))**(-num)))/(Math.sqrt(5));
 
+    //Math.floor rounds down, Math.ceil rounds up
+    return Math.floor(fib); 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
