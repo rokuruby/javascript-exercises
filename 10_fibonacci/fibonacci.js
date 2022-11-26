ce6b3d0b1688ab16012660d4eb0cc6ef41a1c776
Tiong Lee <LLLTTH@gmail.com>
2022-11-26 22:45:57 +0800

    added some comments for explanation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,7 @@
 const fibonacci = function(num) {
   //using Binet's formula so that its not recursive.This is so I can just plug a fib sequence and formula will spit our the fib number isntead of manually calculating every fib number aka reecursive
   
+  //i calculate the golden ratio for more accuracy over using the shorter 1.618
   if (num < 0) {return "OOPS"};
     let fib = ((((1+(Math.sqrt(5)))/2)**(num))-((-((1+(Math.sqrt(5)))/2))**(-num)))/(Math.sqrt(5));
 
