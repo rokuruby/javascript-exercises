1143ff390606b9015d0e7132ca17431cc7d7281d
Tiong Lee <LLLTTH@gmail.com>
2022-11-26 21:56:44 +0800

    finally figured the all the code to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var oriString = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ").join("");
+    var splitString = oriString.split(""); 
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+        console.log(oriString);
+        console.log(joinArray);
 
+    if (oriString == joinArray) {
+        return true;
+    }
+        return false;
 };
 
 // Do not edit below this line
