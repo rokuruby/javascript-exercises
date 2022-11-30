34820890bc7c647468e9d98cacbff30911a08fc0
Tiong Lee <LLLTTH@gmail.com>
2022-11-30 11:30:21 +0800

    couldnt figure out

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const findTheOldest = function(array) {
+        return array.reduce((oldest, currentPerson) => {
+          const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+          const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+          return oldestAge < currentAge ? currentPerson : oldest;
+        })
+      };
+      
+      const getAge = function(birth, death) {
+        if (!death) {
+          death = new Date().getFullYear();
+        }
+        return death - birth;
+      };
 
 };
-
+// copypasta from solutions
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/*https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/ */
